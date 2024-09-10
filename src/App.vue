3b52864b91378612248a5f919ff9cb8f11dd0e9e
Artem-Parashchuk <a.p.paraschuk@gmail.com>
2024-09-10 19:17:43 +0300

    add DeleteIcon & EditIcon components

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,7 @@
 <script setup>
 import HomePageVue from './components/HomePageVue/HomePageVue.vue'
-
-const arr = [1,2,3,4,5,6]
-const obj = {
-  kivi: 'Super kivi',
-  banana: 'Super banana',
-  tomato: 'Super tomato'
-}
 </script>
 
 <template>
     <HomePageVue />
-    <div :key="num" v-for="num in arr">
-      {{ num }}
-    </div>
-
-    <div :key="fruit" v-for="( fruit, key ) in obj">{{key}}: {{ fruit }}</div>
-
-    <div :key="letter" v-for="letter in 'Hello'">{{ letter }}</div>
 </template>
---
+++ src/components/FavoritePlace/FavoritePlace.vue
@@ -0,0 +1,28 @@
+<script setup>
+import FavoritePlaceBtn from './FavoritePlaceBtn.vue'
+import DeleteIcon from './DeleteIcon.vue'
+import EditIcon from './EditIcon.vue'
+</script>
+
+<template>
+  <section class="text-grey mb-6 last:mb-0">
+    <div class="flex gap-4">
+      <img class="w-[76px] h-[76px] shrink-0" src="" alt="" />
+      <div>
+        <div class="flex justify-between items-center mb-2">
+          <h2 class="font-bold text-sm text-[#2c2c2c]">Палац Спорту</h2>
+          <div class="flex gap-2">
+            <FavoritePlaceBtn><EditIcon /></FavoritePlaceBtn>
+            <FavoritePlaceBtn><DeleteIcon /></FavoritePlaceBtn>
+          </div>
+        </div>
+        <p class="text-xs line-clamp-3">
+          Київський палац спорту (КПС) - найбільша крита споруда розташована у центральній частині
+          міста Київ, біля підніжжя
+        </p>
+      </div>
+    </div>
+
+    <div class="h-[1px] w-full bg-[#ececec] mt-4"></div>
+  </section>
+</template>
